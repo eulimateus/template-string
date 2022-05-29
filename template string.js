3eb97c1e2a816e7d57ec67c8fcf4537e9c5dae96
eulimateus <104548335+eulimateus@users.noreply.github.com>
2022-05-29 14:46:09 -0300

    Add files via upload

---
+++ template string.js
@@ -0,0 +1,17 @@
+let nome = 'matheus';
+let sobrenome = 'alves';
+
+ // Sem template string//
+console.log (
+    
+'Olá, me chamo ' + 
+nome + 
+' ' +  
+sobrenome 
+);
+
+
+ // Com template string//
+console.log(`Olá, meu nome é ${nome} ${sobrenome}`)
+
+console.log(`O resultado da soma de 1 + 1 =  ${1 + 1}`)
